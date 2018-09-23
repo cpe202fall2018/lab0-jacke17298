3c1c92741c0bc92b1d718343c437267ad65cad03
Jack <ellingsonjack@gmail.com>
2018-09-22 17:38:49 -0700

    I modified planets.py to complete lab0

--- planets.py
+++ planets.py
@@ -1,7 +1,21 @@
 def weight_on_planets():
-   # write your code here
-   
+    # write your code here
+    print("What do you weigh on earth? ", end='')
+
+    planets = ["Mars", "Jupiter"]
+    wmult = [0.38, 2.34]
+
+    weight = int(input())
+    result = "\n"
+
+    for i in range(2):
+        result += "On " + planets[i] + " you would weigh " + str(wmult[i]*weight) + " pounds."
+
+        if i == 0:
+            result += "\n"
+
+    print(result, end='')
    
    
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
